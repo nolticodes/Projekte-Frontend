5621f3396c6c87b4cc5e45b2207da75228173cde
Denis Nolting <denis.nolting@gmx.de>
2026-03-12 17:32:43 +0100

    added start page for picking quiz

--- QuizApp/js/script_pick_quiz.js
+++ QuizApp/js/script_pick_quiz.js
@@ -3,5 +3,6 @@ let allQuizes = ["quiz_1", "quiz_2", "quiz_3", "quiz_4"]
 function activateStartButton(id) {
     document.getElementById("button_start_quiz_id").disabled = false;
     document.getElementById(id).style = "color: white";
+    
 }
 
